ba667628df41196fe0c3de285b7056743bf20c40
jetsys <275084324@qq.com>
2014-02-14 01:13:29 +0800

    第一期不涉及数据库

---
+++ app/controllers/Application.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    public static void productdes() {
+    	render();
+    }
+    
+    public static void companyintro() {
+    	render();
+    }
+    
+    public static void designcase() {
+    	render();
+    }
+    
+    public static void companyhistory() {
+    	render();
+    }
+    
+    public static void weborder() {
+    	render();
+    }
+    
+    public static void contactus() {
+    	render();
+    }
+}
